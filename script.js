5ee5b1cf59ce00cd29d33911624a8691749f0390
priyabrata1122 <priyabratass64@gmail.com>
2024-07-30 01:02:52 +0530

    Commit Day 3

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ let main_logo = document.querySelector("#com_logo");
 main_logo.addEventListener('click', function(){
     window.location.href = 'index.html';
 })
+
+let get_started = document.querySelector('#get_started');
+get_started.addEventListener('click',function(){
+    window.location.href = 'main.html';
+})
